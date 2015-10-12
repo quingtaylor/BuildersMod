5fb2fad6b1f2c9aa7b34e63e37e761800f1b9a96
AldenTaylor <aldentaylor@gmail.com>
2015-10-11 20:02:51 -0700

    STAIRS

--- main/java/scorchmuffin/mods/builder/BuilderItem.java
+++ main/java/scorchmuffin/mods/builder/BuilderItem.java
@@ -13,9 +13,40 @@ public class BuilderItem extends Item {
 
 	
 	@Override
-	public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side,
-			float hitX, float hitY, float hitZ) {
+	public boolean onItemUse(ItemStack stack, EntityPlayer player,
+			World world, int x, int y, int z, int side, float hitX, float hitY,
+			float hitZ) {
 
+		// buildSquare(world, x, y, z);
+		buildStairs(world, x, y, z);
+		player.inventory.consumeInventoryItem(this);
+		return true;
+	}
+
+	private void buildStairs(World world, int x, int y, int z) {
+		int maxY = y + 50;
+		int i = x;
+		int k = z;
+		for (int j = y; j <= maxY; j++) {
+			world.setBlock(i, j, k, Blocks.glowstone);
+			
+			if (j % 4 == 0) {
+				i++;
+			} else if (j % 4 == 1) {
+				k++;
+			} else if (j % 4 == 2) {
+				i--;
+			} else if (j % 4 == 3) {
+				world.setBlock(i+1, j, k, Blocks.torch);
+				k--;
+			}
+			if (j %34 == 0) {
+				world.setBlock(i+1, j+1, k, Blocks.lava);
+			}
+		}
+	}
+
+	private void buildSquare(World world, int x, int y, int z) {
 		int maxX = x + 5;
 		int maxY = y + 8;
 		int maxZ = z + 10;
@@ -40,8 +71,6 @@ public class BuilderItem extends Item {
 				world.setBlock(i, maxY, k, getBlock(i, y, k));
 			}
 		}
-		player.inventory.consumeInventoryItem(this);
-		return true;
 	}
 
 	private Block getBlock(int x, int y, int z) {
