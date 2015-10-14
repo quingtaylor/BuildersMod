0c04f2c52c151f65e7648938b684f2c0ffc0f4f9
quingtaylor <quinlan@Amary-Taylors-MacBook-Pro.local>
2015-10-13 20:07:19 -0700

    added build room function
    using buildFloor buildWallX and buildWallZ

--- main/java/scorchmuffin/mods/builder/Buildingrod.java
+++ main/java/scorchmuffin/mods/builder/Buildingrod.java
@@ -14,7 +14,9 @@ public class Buildingrod extends Item {
 	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side,
 			float hitX, float hitY, float hitZ) {
 
-		buildFloor(world, x, y, z);
+		// buildFloor(world, x, y, z);
+		// buildWall(world, x, y, z);
+		buildRoom(world, x, y, z);
 
 		player.inventory.consumeInventoryItem(this);
 		return true;
@@ -32,20 +34,53 @@ public class Buildingrod extends Item {
 		}
 	}
 
-//	private void buildFloor2(World world, int x, int y, int z) {
-//		int maxX = x + 8;
-//		int maxY = y;
-//		int maxZ = z + 8;
-//
-//		for (int i = x; i <= maxX; i = i + 4) {
-//			for (int k = z; k <= maxZ; k++) {
-//				world.setBlock(i, y, k, Blocks.glowstone);
-//				world.setBlock(i + 1, y, k, Blocks.brick_block);
-//				world.setBlock(i + 2, y, k, Blocks.emerald_ore);
-//				world.setBlock(i + 3, y, k, Blocks.brick_block);
-//			}
-//		}
-//	}
+	private void buildWallX(World world, int x, int y, int z) {
+		int maxX = x + 8;
+		int maxY = y + 5;
+		int maxZ = z;
+
+		for (int i = x; i <= maxX; i++) {
+			for (int k = y; k <= maxY; k++) {
+				world.setBlock(i, k, z, getBlock(i, k, z));
+			}
+		}
+	}
+
+	private void buildWallZ(World world, int x, int y, int z) {
+		int maxX = x;
+		int maxY = y + 5;
+		int maxZ = z + 8;
+
+		for (int i = z; i <= maxZ; i++) {
+			for (int k = y; k <= maxY; k++) {
+				world.setBlock(x, k, i, getBlock(x, k, i));
+			}
+		}
+	}
+
+	private void buildRoom(World world, int x, int y, int z) {
+		buildFloor(world, x, y, z);
+		buildWallX(world, x, y, z);
+		buildWallX(world, x, y, z + 8);
+		buildFloor(world, x, y + 5, z);
+		buildWallZ(world, x, y, z);
+		buildWallZ(world, x + 8, y, z);
+	}
+
+	// private void buildFloor2(World world, int x, int y, int z) {
+	// int maxX = x + 8;
+	// int maxY = y;
+	// int maxZ = z + 8;
+	//
+	// for (int i = x; i <= maxX; i = i + 4) {
+	// for (int k = z; k <= maxZ; k++) {
+	// world.setBlock(i, y, k, Blocks.glowstone);
+	// world.setBlock(i + 1, y, k, Blocks.brick_block);
+	// world.setBlock(i + 2, y, k, Blocks.emerald_ore);
+	// world.setBlock(i + 3, y, k, Blocks.brick_block);
+	// }
+	// }
+	// }
 
 	private Block getBlock(int x, int y, int z) {
 		if (x % 4 == 0)
